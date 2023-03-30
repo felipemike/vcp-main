f04cc11707274203f5e486744d7009e72349f8f2
felipemike <felipeclemente2012@gmail.com>
2023-03-30 16:27:56 -0300

    melhoria do codigo do index.php

--- index.php
+++ index.php
@@ -1,31 +1,33 @@
 <?php
-	require('_inc_/config.php');
-	require('_inc_/vcp.class.php');
-	
-	$vcp = new VCP();
-	
-	$vcp->setPageTitle('VCP - Vote e Conquiste Pontos');
-	$vcp->setPageDescription('Vote e Conquiste Pontos');
-	$vcp->setPageKeywords('Vote, Conquiste, Pontos');
+require '_inc_/config.php';
+require '_inc_/vcp.class.php';
 
-require('_inc_/header.php');
-	echo '<strong class="c1">Seus pontos:</strong> ', $vcp->getPoints(), '<br /><br />';
-	
-	$tops = $vcp->getTops(true);
-	if(!$tops) {
-		$vcp->setFlash('N�o h� tops registrados', 'info');
-	} else {
-	?>
-		<div id="selecionar-top">
-		Top: 
-		<select id="tops">
-		<option value="0">Selecione</option>
-		<?php echo $tops; ?>
-		</select>
-		</div>
-		<?php $vcp->setFlash('Selecione o TOP que deseja vizualizar e vote para ganhar pontos', 'info'); ?>
-	<?php
-	}
-	
-	require('_inc_/footer.php');
-	?>
+$vcp = new VCP();
+
+// Set page metadata
+$vcp->setPageTitle('VCP - Vote e Conquiste Pontos');
+$vcp->setPageDescription('Vote e Conquiste Pontos');
+$vcp->setPageKeywords('Vote, Conquiste, Pontos');
+
+// Load header
+require '_inc_/header.php';
+
+// Display user's points
+echo '<strong class="c1">Seus pontos:</strong> ' . $vcp->getPoints() . '<br /><br />';
+
+// Display tops and allow user to select one
+$tops = $vcp->getTops(true);
+if (!$tops) {
+  $vcp->setFlash('Não há tops registrados', 'info');
+} else {
+  echo '<div id="selecionar-top">';
+  echo 'Top: <select id="tops">';
+  echo '<option value="0">Selecione</option>';
+  echo $tops;
+  echo '</select></div>';
+  $vcp->setFlash('Selecione o TOP que deseja visualizar e vote para ganhar pontos', 'info');
+}
+
+// Load footer
+require '_inc_/footer.php';
+?>
