e5f7f3d6167f3f5534096b063481990e8bf1b362
felipemike <felipeclemente2012@gmail.com>
2023-03-30 16:30:21 -0300

    melhoria do codigo do edit-top.php

--- edit-top.php
+++ edit-top.php
@@ -1,27 +1,41 @@
 <?php
-	require('_inc_/config.php');
-	require('_inc_/functions.php');
-	require('_inc_/class.vcp.php');
-	$vcp = new vcp();
+define('INC_PATH', '_inc_/');
+require(INC_PATH . 'config.php');
+require(INC_PATH . 'functions.php');
+require(INC_PATH . 'class.vcp.php');
 
-require('_inc_/header.php');
-	$vcp->levelCheck();
-	if($vcp->getTopInfos($_GET['id'])) {
-	$x = $vcp->index[0];
-	?>
-	<h3>Editar TOP</h3><br />
-	<form action="edit-top.php?id=<?php echo $_GET['id']; ?>" method="post">
-	<label for="nome">Nome do TOP:</label> <input type="text" name="nome" id="nome" value="<?php echo (isset($_POST['nome'])) ? $_POST['nome'] : $x['nome']; ?>" />
-	<label for="url">URL do TOP:</label> <input type="text" name="url" id="url" value="<?php echo (isset($_POST['url'])) ? $_POST['url'] : $x['url']; ?>" />
-	<label for="minutos">Minutos para votar novamente:</label> <input type="text" name="minutos" id="minutos" value="<?php echo (isset($_POST['minutos'])) ? $_POST['minutos'] : $x['minutos']; ?>" />
-	<label for="pontos">Pontos ganhos por voto:</label> <input type="text" name="pontos" id="pontos" value="<?php echo (isset($_POST['pontos'])) ? $_POST['pontos'] : $x['pontos']; ?>" /><br />
-	<input type="image" src="images/bt-enviar.png" class="bt" />
-	</form>
+$vcp = new VCP($_GET['id']);
 
-	<?php 
-	$vcp->addTop($_GET['id']);
-	} else {
-		$vcp->setFlash(error_9, 'error');
-	}
-	require('_inc_/footer.php'); 
-	?>
+require(INC_PATH . 'header.php');
+
+$vcp->levelCheck();
+
+if ($vcp->getTopInfos()) {
+    $x = $vcp->index[0];
+    $vcp->addTop();
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $vcp->processEditForm($_POST);
+    }
+
+    ?>
+    <h3>Editar TOP</h3><br />
+    <form action="edit-top.php?id=<?php echo htmlspecialchars($_GET['id']); ?>" method="post">
+        <label for="nome">Nome do TOP:</label>
+        <input type="text" name="nome" id="nome" value="<?php echo htmlspecialchars(isset($_POST['nome']) ? $_POST['nome'] : $x['nome']); ?>" />
+        <label for="url">URL do TOP:</label>
+        <input type="text" name="url" id="url" value="<?php echo htmlspecialchars(isset($_POST['url']) ? $_POST['url'] : $x['url']); ?>" />
+        <label for="minutos">Minutos para votar novamente:</label>
+        <input type="text" name="minutos" id="minutos" value="<?php echo htmlspecialchars(isset($_POST['minutos']) ? $_POST['minutos'] : $x['minutos']); ?>" />
+        <label for="pontos">Pontos ganhos por voto:</label>
+        <input type="text" name="pontos" id="pontos" value="<?php echo htmlspecialchars(isset($_POST['pontos']) ? $_POST['pontos'] : $x['pontos']); ?>" /><br />
+        <input type="image" src="images/bt-enviar.png" class="bt" />
+    </form>
+
+    <?php
+} else {
+    $vcp->setFlash(ERROR_9, 'error');
+}
+
+require(INC_PATH . 'footer.php');
+?>
